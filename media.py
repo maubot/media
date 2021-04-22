de733ca734cea0f7851e8dfaba6bfb3dfd303989
euri10 <benoit.barthelet@gmail.com>
2021-04-22 12:11:16 +0200

    Allow encrypted files

--- media.py
+++ media.py
@@ -6,7 +6,7 @@ from maubot.handlers import command
 
 
 class MediaBot(Plugin):
-    @command.passive("^mxc://.+/.+$", field=lambda evt: evt.content.url,
+    @command.passive("^mxc://.+/.+$", field=lambda evt: evt.content.url if evt.content.url else evt.content.file.url,
                      msgtypes=(MessageType.IMAGE, MessageType.FILE, MessageType.AUDIO,
                                MessageType.STICKER))
     async def handler(self, evt: MessageEvent, url: Tuple[str]) -> None:
